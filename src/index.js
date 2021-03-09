89501e7d4ff74abb51e80f27180480d7c3d2a04b
Dina Shchobova <dina.shchobova@gmail.com>
2021-03-09 23:13:07 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (array !== undefined) && (array.length !== 0) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array !== undefined) && (array.length !== 0) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let avg = 0;
+    if ((array !== undefined) && (array.length !== 0)) {
+
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        avg = sum / array.length;
+    }
+    return avg;
 }
